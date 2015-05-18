7157c111e735303d0937bd07cb5cea7f0b0274d9
Sulaiman Azhar <ssk970@gmail.com>
2015-05-19 03:08:56 +0400

    Replace Font HelveticaNeue-CondensedBlack with Impact

--- MemeMe/MemeEditorViewController.swift
+++ MemeMe/MemeEditorViewController.swift
@@ -32,7 +32,7 @@ class MemeEditorViewController: UIViewController,
         let memeTextAttributes = [
             NSStrokeColorAttributeName: UIColor.blackColor(),
             NSForegroundColorAttributeName: UIColor.whiteColor(),
-            NSFontAttributeName: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            NSFontAttributeName: UIFont(name: "Impact", size: 40)!,
             NSStrokeWidthAttributeName: -3.0
         ]
         
