74664b44ff2c3b25a65a084754be0f92bab7c621
Sulaiman Azhar <ssk970@gmail.com>
2015-05-18 13:42:22 +0400

    Slide view up to allow editing the bottom text field
    The entire view slides up during editing, and back after the keyboard
    is dismissed.

--- MemeMe/MemeEditorViewController.swift
+++ MemeMe/MemeEditorViewController.swift
@@ -39,6 +39,9 @@ class MemeEditorViewController: UIViewController,
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         
+        // Subscribe to the keyboard notifications, to allow the view to raise when necessary
+        self.subscribeToKeyboardNotification()
+        
         cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(
             UIImagePickerControllerSourceType.Camera)
         
@@ -47,6 +50,10 @@ class MemeEditorViewController: UIViewController,
     
     override func viewWillDisappear(animated: Bool) {
         super.viewWillDisappear(animated)
+        
+        // Unsubscribe from the keyboard notifications.
+        self.unsubscribeFromKeyboardNotification()
+        
         self.tabBarController?.tabBar.hidden = false;
     }
     
@@ -86,11 +93,48 @@ class MemeEditorViewController: UIViewController,
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     
-    // MARK: - Text Field Delegate
+    // MARK: - Text Field Delegate and Notifications
     
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
+    }
+    
+    func subscribeToKeyboardNotification() {
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:",
+            name: UIKeyboardWillShowNotification, object: nil)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide",
+            name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotification() {
+        
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
+        
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+    }
+    
+    // Move the view when the keyboard covers the text field
+    func keyboardWillShow(notification: NSNotification) {
+        
+        if (self.bottomTextField.isFirstResponder()) {
+            self.view.frame.origin.y -= getKeyboardHeight(notification)
+        }
+    }
+    
+    func getKeyboardHeight(notification: NSNotification) -> CGFloat {
+        
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.CGRectValue().height
+    }
+    
+    func keyboardWillHide() {
+        
+        // reset frame position
+        self.view.frame.origin.y = 0
     }
 
 }
