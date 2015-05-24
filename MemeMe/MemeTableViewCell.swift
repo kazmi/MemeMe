9af58c39c799c192c86064d49ab665e073d80f1d
Sulaiman Azhar <ssk970@gmail.com>
2015-05-24 23:34:18 +0400

    Remove unused methods from MemeTableViewCell

--- MemeMe/MemeTableViewCell.swift
+++ MemeMe/MemeTableViewCell.swift
@@ -13,15 +13,4 @@ class MemeTableViewCell: UITableViewCell {
     @IBOutlet weak var memedImageView: UIImageView!
     @IBOutlet weak var memeTextLabel: UILabel!
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }
